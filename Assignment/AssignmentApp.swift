2cda08dfe5cf09efeec25300941af33456a5d07f
Saurabh <developer@kauverymeds.com>
2025-10-19 18:41:58 +0530

    Initial Commit

---
+++ Assignment/AssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  AssignmentApp.swift
+//  Assignment
+//
+//  Created by Saurabh Mahavarkar on 19/10/25.
+//
+
+import SwiftUI
+
+@main
+struct AssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Assignment/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Assignment
+//
+//  Created by Saurabh Mahavarkar on 19/10/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
